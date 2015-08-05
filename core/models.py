25816ef86ccfe521d7958bd33d874330db635dd6
natakopulenko <nata.kopulenko@mail.ru>
2015-08-05 12:24:08 +0300

    commit

--- core/models.py
+++ core/models.py
@@ -15,3 +15,8 @@ class Service(models.Model):
     title = models.CharField(max_length=50)
     type = models.ForeignKey(ServiceType)
     address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    description = models.TextField()
+    information = models.TextField()
+    site = models.URLField(max_length=200)
+    mark = models.DecimalField(max_digits=3, decimal_places=2)
