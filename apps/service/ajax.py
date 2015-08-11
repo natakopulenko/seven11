300f83e7bd7fff85c822c380d749b428a48b800d
natakopulenko <nata.kopulenko@mail.ru>
2015-08-11 13:20:26 +0300

    angular

--- apps/service/ajax.py
+++ apps/service/ajax.py
@@ -34,20 +34,60 @@ class PostView(View):
             return HttpResponseBadRequest('parameter {0} is invalid'.format(parameter_name))
 
         @classmethod
-        def ok(cls, post):
+        def ok(cls, post, items):
             return JsonResponse({'code': 0,
                                  'message': 'OK',
                                  'data': {
                                      'title': post.title,
                                      'category': post.category.title,
                                      'id': post.id,
-                                     'image': post.main_image.url,
+                                     'main_image': post.main_image.url,
+                                     'items': [{
+                                         'image':  item['image'],
+                                         'text': item['text'],
+                                         'subtitle': item['subtitle'],
+                                     } for item in items],
+
                                  }
                                  })
 
     def get(self, request, post_id):
         post = Post.objects.get(id=post_id)
-        return self.PostCodes.ok(post)
+        post_text = PostText.objects.filter(post=post_id)
+        post_images = PostImage.objects.filter(post=post_id)
+        post_subtitles = PostSubtitle.objects.filter(post=post_id)
+        items_time = []
+        items = []
+        for text in post_text:
+            items_time.append(text.time_of_adding)
+        for image in post_images:
+            items_time.append(image.time_of_adding)
+        for subtitle in post_subtitles:
+            items_time.append(subtitle.time_of_adding)
+        items_time.sort()
+        for time in items_time:
+            texts = post_text.filter(time_of_adding=time)
+            text = None
+            image = None
+            subtitle = None
+            if texts is not None:
+                for text_item in texts:
+                    text = text_item.text
+
+            images = post_images.filter(time_of_adding=time)
+            if images is not None:
+                for image_item in images:
+                    image = image_item.image.url
+            subtitles = post_subtitles.filter(time_of_adding=time)
+            if subtitles is not None:
+                for subtitle_item in subtitles:
+                    subtitle = subtitle_item.subtitle
+            items.append({
+                'image': image,
+                'text': text,
+                'subtitle': subtitle,
+            })
+        return self.PostCodes.ok(post, items)
 
 
 class CategoryView(View):
